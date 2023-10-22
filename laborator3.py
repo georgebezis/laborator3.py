2e241ac0dc7eb735ba9c6e4e8f5763740b859622
georgebezis <georgebezis31@gmail.com>
2023-10-22 14:06:34 +0300

    cc

--- laborator3.py
+++ laborator3.py
@@ -15,10 +15,10 @@ while True:
     points_trapez=np.array([upper_right,upper_left,lower_left,lower_right],dtype=np.int32)
     frame2=cv2.fillConvexPoly(frame2,points_trapez,1)
     frame3=frame2*frame
-    
+    print("pc")
 
     if ret is False:
-        print('1')
+
         break
     cv2.imshow('Original', frame)
     cv2.imshow('trapez',frame2)
