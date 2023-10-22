9a8a7120f78f91613ac2146aab46ffe7ac462279
georgebezis <georgebezis31@gmail.com>
2023-10-22 23:20:51 +0300

    cc

--- laborator3.py
+++ laborator3.py
@@ -12,17 +12,26 @@ while True:
     upper_right=(int(640*0.55),int(400*0.73))
     lower_left=(0,400)
     lower_right=(640,400)
-    points_trapez=np.array([upper_right,upper_left,lower_left,lower_right],dtype=np.int32)
-    frame2=cv2.fillConvexPoly(frame2,points_trapez,1)
+    bounds_trapez=np.array([upper_right,upper_left,lower_left,lower_right],dtype=np.int32)
+    frame2=cv2.fillConvexPoly(frame2,bounds_trapez,1)
     frame3=frame2*frame
+    bounds_trapez=np.float32(bounds_trapez)
+    bounds_screen=np.array([(640,0),(0,0),(0,400),(640,400)])
+    bounds_screen=np.float32(bounds_screen)
+    magic_matrix=cv2.getPerspectiveTransform(bounds_trapez,bounds_screen)
+    frame3=cv2.warpPerspective(frame3,magic_matrix,(640,400))
+    frame4=cv2.blur(frame3,(7,7))
+
+
 
 
     if ret is False:
 
         break
     cv2.imshow('Original', frame)
-    cv2.imshow('trapez',frame2)
+    #cv2.imshow('trapez',frame2)
     cv2.imshow('road',frame3)
+    cv2.imshow('blured',frame4)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cam.release()
