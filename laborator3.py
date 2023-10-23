dc492f5e4ab8cf13d30ab9168d6fc8b291e75ad0
georgebezis <georgebezis31@gmail.com>
2023-10-23 22:07:13 +0300

    cc

--- laborator3.py
+++ laborator3.py
@@ -6,32 +6,45 @@ while True:
     ret, frame = cam.read()
     frame = cv2.resize(frame,(640,400))
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame2=np.zeros(frame.shape,dtype=np.uint8)
+    trapez=np.zeros(frame.shape,dtype=np.uint8)
 
-    upper_left=(int(640*0.45),int(400*0.73))
-    upper_right=(int(640*0.55),int(400*0.73))
+    upper_left=(int(640*0.45),int(400*0.79))
+    upper_right=(int(640*0.55),int(400*0.79))
     lower_left=(0,400)
     lower_right=(640,400)
     bounds_trapez=np.array([upper_right,upper_left,lower_left,lower_right],dtype=np.int32)
-    frame2=cv2.fillConvexPoly(frame2,bounds_trapez,1)
-    frame3=frame2*frame
+    cv2.fillConvexPoly(trapez,bounds_trapez,1)
+    road=trapez*frame
     bounds_trapez=np.float32(bounds_trapez)
     bounds_screen=np.array([(640,0),(0,0),(0,400),(640,400)])
     bounds_screen=np.float32(bounds_screen)
     magic_matrix=cv2.getPerspectiveTransform(bounds_trapez,bounds_screen)
-    frame3=cv2.warpPerspective(frame3,magic_matrix,(640,400))
-    frame4=cv2.blur(frame3,(7,7))
+    stretched=cv2.warpPerspective(road,magic_matrix,(640,400))
+    blur=cv2.blur(stretched,(3,3))
+    sobel_vertical=np.float32([[-1, -2, -1],[0,0,0],[1,2,1]])
+    sobel_horizontal=np.transpose(sobel_vertical)
+    blur=np.float32(blur)
+    sobelHorizontal=cv2.filter2D(blur,-1,sobel_horizontal,)
+    sobelVertical=cv2.filter2D(blur,-1,sobel_vertical)
+    sobel=((sobelVertical)**2+(sobelHorizontal)**2)**1/2
 
 
 
-
+    sobelHorizontal=cv2.convertScaleAbs(sobelHorizontal)
+    sobelVertical = cv2.convertScaleAbs(sobelVertical)
+    blur=cv2.convertScaleAbs(blur)
+    sobel=cv2.convertScaleAbs(sobel)
     if ret is False:
 
         break
     cv2.imshow('Original', frame)
-    #cv2.imshow('trapez',frame2)
-    cv2.imshow('road',frame3)
-    cv2.imshow('blured',frame4)
+    cv2.imshow('trapez',trapez)
+    cv2.imshow('road',road )
+    cv2.imshow("stretched",stretched)
+    cv2.imshow('blured',blur)
+    cv2.imshow("sobelhorizontal",sobelHorizontal)
+    cv2.imshow("sobelvertical", sobelVertical)
+    cv2.imshow("sobel", sobel)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cam.release()
