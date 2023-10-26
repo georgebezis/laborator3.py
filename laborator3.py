073162583fa237e1f15d5fa13a606afc2c56a222
georgebezis <georgebezis31@gmail.com>
2023-10-26 20:41:26 +0300

    cc

--- laborator3.py
+++ laborator3.py
@@ -2,56 +2,58 @@ import cv2
 import numpy as np
 
 cam = cv2.VideoCapture('Lane Detection Test Video-01.mp4')
-aux_left_top = 0
-aux_left_bottom = 0
-aux_right_top = 0
-aux_right_bottom = 0
+
 left_top=[0,0]
 left_bottom=[0,0]
 right_top=[0,0]
 right_bottom=[0,0]
 while True:
     ret, frame = cam.read()
-    frame = cv2.resize(frame,(640,400))
+    frame_h=400
+    frame_w=640
+    frame = cv2.resize(frame,(frame_w,frame_h))
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     trapez=np.zeros(frame.shape,dtype=np.uint8)
 
-    upper_left=(int(640*0.45),int(400*0.775))
-    upper_right=(int(640*0.55),int(400*0.775))
-    lower_left=(0,390)
-    lower_right=(640,390)
+    upper_left=(int(frame_w*0.45),int(frame_h*0.76))
+    upper_right=(int(frame_w*0.55),int(frame_h*0.76))
+    lower_left=(0,frame_h-int(frame_h*0.0))
+    lower_right=(frame_w,frame_h-int(frame_h*0.0))
     bounds_trapez=np.array([upper_right,upper_left,lower_left,lower_right],dtype=np.int32)
     cv2.fillConvexPoly(trapez,bounds_trapez,1)
     road=trapez*frame
     bounds_trapez=np.float32(bounds_trapez)
-    bounds_screen=np.array([(640,0),(0,0),(0,400),(640,400)])
+    bounds_screen=np.array([(frame_w,0),(0,0),(0,frame_h),(frame_w,frame_h)])
     bounds_screen=np.float32(bounds_screen)
     magic_matrix=cv2.getPerspectiveTransform(bounds_trapez,bounds_screen)
-    stretched=cv2.warpPerspective(road,magic_matrix,(640,400))
-    blur=cv2.blur(stretched,(3,3))
+    stretched=cv2.warpPerspective(road,magic_matrix,(frame_w,frame_h))
+    blur=cv2.blur(stretched,(5,7))
     sobel_vertical=np.float32([[-1, -2, -1],[0,0,0],[1,2,1]])
     sobel_horizontal=np.transpose(sobel_vertical)
     blur=np.float32(blur)
     sobelHorizontal=cv2.filter2D(blur,-1,sobel_horizontal,)
     sobelVertical=cv2.filter2D(blur,-1,sobel_vertical)
     sobel=np.sqrt((sobelVertical)**2+(sobelHorizontal)**2)
-    threshold=int(255/4)
+    threshold=int(255/7)
     #thresholdframe=np.where(sobel<threshold,0,255)
     aux,thresholdframe=cv2.threshold(sobel,threshold,255,cv2.THRESH_BINARY)
     #9
     thresholdframe_copy=np.copy(thresholdframe)
-    thresholdframe_copy[0:400,0:int(640*0.05)]=0
-    thresholdframe_copy[0:400,640-int(640 * 0.05):640] = 0
+    thresholdframe_copy[0:frame_h,0:int(frame_w*0.05)]=0
+    thresholdframe_copy[0:frame_h,frame_w-int(frame_w * 0.05):frame_w] = 0
+    thresholdframe_copy[frame_h-int(frame_h*0.03):frame_h, 0:frame_w] = 0
     #thresholdframe_copy[int(0.001*400):400,0:640]=0
-    leftpart=np.argwhere(thresholdframe_copy[0:400,0:int(640/2)]>1)
-    rightpart=np.argwhere(thresholdframe_copy[0:400,int(640/2):640])
+    leftpart=np.copy(thresholdframe_copy[0:frame_h,0:int(frame_w/2)])
+    leftpart=np.argwhere(leftpart)
+    rightpart=np.copy(thresholdframe_copy[0:frame_h,int(frame_w/2):frame_w])
+    rightpart=np.argwhere(rightpart)
 
     #print(rightpart[0])
 
 
     left_xs=leftpart[:,1]
     left_ys=leftpart[:,0]
-    right_xs=rightpart[:,1]+640/2
+    right_xs=rightpart[:,1]+frame_w/2
     right_ys=rightpart[:,0]
     #10
     lb,la=np.polynomial.polynomial.polyfit(left_xs,left_ys,1)
@@ -60,39 +62,33 @@ while True:
 
     left_top_y=0
     left_top_x=(left_top_y-lb)/la
-    left_bottom_y=400
+    left_bottom_y=frame_h
     left_bottom_x=(left_bottom_y-lb)/la
 
     right_top_y = 0
     right_top_x = (right_top_y - rb) / ra
-    right_bottom_y = 400
+    right_bottom_y = frame_h
     right_bottom_x = (right_bottom_y- rb) / ra
 
 
          #c
 
-    aux_left_top = left_top_x
-
-    aux_left_bottom = left_bottom_x
-
-    aux_right_top = right_top_x
-
-    aux_right_bottom = right_bottom_x
     if (-(10 **3)) < left_top_x < (10 ** 3):
-     left_top=[int(aux_left_top),int(left_top_y)]
+     left_top=[int(left_top_x),int(left_top_y)]
     if (-(10 ** 3)) < left_bottom_x < (10 ** 3):
-     left_bottom = [int(aux_left_bottom), int(left_bottom_y)]
+     left_bottom = [int(left_bottom_x), int(left_bottom_y)]
     if (-(10 ** 3)) < right_top_x < (10 ** 3):
-     right_top = [int(aux_right_top), int(right_top_y)]
+     right_top = [int(right_top_x), int(right_top_y)]
     if (-(10 ** 3)) < right_bottom_x < (10 ** 3):
-     right_bottom = [int(aux_right_bottom), int(right_bottom_y)]
+     right_bottom = [int(right_bottom_x), int(right_bottom_y)]
 
-    print(right_top)
+    #print(right_top)
+    print(right_bottom)
 
     lines=np.copy(thresholdframe_copy)
     lines=cv2.line(lines,left_top,left_bottom,(200,0,0),10)
     lines=cv2.line(lines,right_top,right_bottom,(100,0,0),5)
-    #lines=cv2.line(lines,(400,int(640*0.5)),(0,int(640*0.5)),(255,0,0),1)
+    lines=cv2.line(lines,(int(640/2),0),(int(640/2),400),(255,0,0),1)
 
     sobelHorizontal=cv2.convertScaleAbs(sobelHorizontal)
     sobelVertical = cv2.convertScaleAbs(sobelVertical)
@@ -115,7 +111,7 @@ while True:
     cv2.imshow("thresh",thresholdframe )
     cv2.imshow("thresh_copy", thresholdframe_copy)
     cv2.imshow("lines", lines)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 cam.release()
 cv2.destroyAllWindows()
